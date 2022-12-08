64950685101b7b7c64a3598fb22949a63b09a45b
Adarsh Raut <adarshraut0987@gmail.com>
2022-12-08 23:31:20 +0530

    first commit

---
+++ src/api/index.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+const URL =
+  "https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary";
+
+const options = {
+  params: {
+    bl_latitude: "11.847676",
+    tr_latitude: "12.838442",
+    bl_longitude: "109.095887",
+    tr_longitude: "109.149359",
+  },
+  headers: {
+    "X-RapidAPI-Key": "e23cf2d5ffmshef1727f2d45d929p1fe10bjsn33fa265fed86",
+    "X-RapidAPI-Host": "travel-advisor.p.rapidapi.com",
+  },
+};
+
+export const getPlacesData = async () => {
+  try {
+    const {
+      data: { data },
+    } = await axios.get(URL, options);
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
